5e2dc7b16cd2ae35f3afa0cf426ee256ccdf1504
Keita Urashima <ursm@ursm.jp>
2021-12-07 02:58:00 +0900

    Styling

--- src/submission-wizard.ts
+++ src/submission-wizard.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { configureLocalization, msg, localized } from '@lit/localize';
 
@@ -25,6 +25,70 @@ type Locale = 'en' | 'ja';
 @localized()
 @customElement('submission-wizard')
 export class SubmissionWizard extends LitElement {
+  static styles = css`
+    :host {
+      --text-color: #1c1b19;
+      --link-color: #0d6efd;
+      --link-hover-color: #0a58ca;
+
+      color: var(--text-color);
+    }
+
+    * {
+      margin: 0;
+      padding: 0;
+    }
+
+    a {
+      color: var(--link-color);
+    }
+
+    a:hover {
+      color: var(--link-hover-color);
+    }
+
+    .stack > * + * {
+      margin-top: 0.75rem;
+    }
+
+    .box {
+      padding: 0.75rem 1rem;
+    }
+
+    .cluster {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 1rem;
+      list-style: none;
+    }
+
+    .border {
+      border: solid thin var(--text-color);
+    }
+
+    .border-top {
+      border-top: solid thin var(--text-color);
+    }
+
+    .bg-light {
+      background-color: hsl(53deg, 100%, 93%);
+    }
+
+    .choice-button {
+      display: inline-block;
+      padding: 0.5em 1.25em;
+      border-radius: 0.25rem;
+      background-color: hsl(27deg, 100%, 50%);
+      color: white !important;
+      text-decoration: none;
+      transition: background-color .15s;
+    }
+
+    .choice-button:hover {
+      background-color: hsl(27deg, 100%, 45%);
+    }
+  `;
+
   set locale(newVal: Locale) {
     const oldVal = getLocale();
 
@@ -46,18 +110,24 @@ export class SubmissionWizard extends LitElement {
 
   render() {
     return html`
-      ${this.renderAnswers()}
-      ${this.renderCurrentNode()}
+      <div class="stack border">
+        ${this.renderAnswers()}
+        ${this.renderCurrentNode()}
+      </div>
     `;
   }
 
   renderAnswers() {
     return this.answers.map(([question, choice]) => {
       return html`
-        <p>
-          ${this.localize(question.text)}: ${this.localize(choice.label)}
-          <small>(<a @click=${this.backTo(question)} href="#">${msg('Change')}</a>)</small>
-        </p>
+        <div>
+          <p class="box bg-light">${this.localize(question.text)}</p>
+
+          <p class="box">
+            ${this.localize(choice.label)}
+            <small><a @click=${this.backTo(question)} href="#">${msg('Change')}</a></small>
+          </p>
+        </div>
       `;
     });
   }
@@ -68,27 +138,29 @@ export class SubmissionWizard extends LitElement {
         const question = this.currentNode;
 
         return html`
-          <p>${this.localize(question.text)}</p>
+          <div>
+            <p class="box bg-light">${this.localize(question.text)}</p>
 
-          <ul>
-            ${question.choices.map((choice: Choice) => {
-              return html`
-                <li>
-                  <a @click=${this.answer(question, choice)} href="#">${this.localize(choice.label)}</a>
-                </li>
-              `;
-            })}
-          </ul>
+            <ul class="box cluster">
+              ${question.choices.map((choice: Choice) => {
+                return html`
+                  <li>
+                    <a @click=${this.answer(question, choice)} href="#" class="choice-button">${this.localize(choice.label)}</a>
+                  </li>
+                `;
+              })}
+            </ul>
+          </div>
         `;
       }
       case 'result': {
         const {repository} = this.currentNode;
 
         return html`
-          <hr>
-
-          <p>${msg('You can submit your data to the following database:')}</p>
-          <a href=${repository.url}>${repository.name}</a> - ${repository.description}
+          <div class="box border-top">
+            <p>✅ ${msg('You can submit your data to the following database:')}</p>
+            <a href=${repository.url}>${repository.name}</a> - ${repository.description}
+          </div>
         `;
       }
       default: {
