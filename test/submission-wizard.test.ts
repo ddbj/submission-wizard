5162cee496bea684b9a326aa7017c520b22864dc
Keita Urashima <ursm@ursm.jp>
2021-12-11 05:50:12 +0900

    src/ -> app/

--- test/submission-wizard.test.ts
+++ test/submission-wizard.test.ts
@@ -1,5 +1,5 @@
 import { expect, fixture, html } from '@open-wc/testing';
-import '../src/submission-wizard.ts';
+import '../app/submission-wizard.ts';
 
 it('initial render', async () => {
   const el = await fixture(html`<submission-wizard></submission-wizard>`);
